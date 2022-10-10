be044147bfc5210c5a2a317fd9b0cdf76f6c65ca
Derfuu <qwesterseven123456@gmail.com>
2022-10-10 13:43:06 +0300

    new V

---
+++ 158/WSUniversalLib/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace WSUniversalLib
+{
+    public class Calculation
+    {
+        int GetQuantityForProduct(int productType, int materialType, int count, float width, float length)
+        {
+            //MAGIC
+            return 0;
+        }
+    }
+}
