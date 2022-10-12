45d23a3becccc1dbafd71f9beec87c8f3440bcb0
Derfuu <qwesterseven123456@gmail.com>
2022-10-12 17:28:03 +0300

    AAAA

--- 148/PriyatniyShelestWPF/AgentsWindow.xaml.cs
+++ 148/PriyatniyShelestWPF/AgentsWindow.xaml.cs
@@ -31,56 +31,99 @@ namespace PriyatniyShelestWPF
         Agent[] agents = new Agent[0];
         int possibleRows = 10;
 
+        SolidColorBrush bgcolor = new SolidColorBrush(Color.FromArgb(0xFF, 0xC6, 0xD7, 0xFF));
+
         string connStr = "Data Source=DESKTOP-0000001; Initial Catalog=priyatniyDEV; Integrated Security=TRUE";
 
         /*
          FUNCTIONS
         */
 
-        void updateTableConfiguretion()
+        void updateTableConfiguretion(int page=1)
         {
             double rowThikness = 150;
             
             centerGrid.RowDefinitions.Clear();
 
-            for (int row = 0; row < possibleRows; row++)
-            {
-                RowDefinition rowDef = new RowDefinition();
-                Border agentDataBorder = new Border();
-                Label agentNameLabel = new Label();
-                Label agentDiscountLabel = new Label();
+            int[] agentsSlice = new int[2];
+            agentsSlice[0] = (page - 1) * 10;
+            agentsSlice[1] = page * 10;
 
-                rowDef.MinHeight = rowThikness;
-                rowDef.MaxHeight = rowThikness;
-                rowDef.Name = $"row{row}";
+            int displayedRows = agentsSlice[1];
+
+            for (int row = agentsSlice[0]; row < displayedRows; row++)
+            {
+                Grid innerGrid = new Grid();
+                innerGrid.HorizontalAlignment = HorizontalAlignment.Stretch;
+                ColumnDefinition imageColumn = new ColumnDefinition();
+                ColumnDefinition descriptionColumn = new ColumnDefinition();
+                innerGrid.ColumnDefinitions.Add(imageColumn);
+                innerGrid.ColumnDefinitions.Add(descriptionColumn);
+                
+                imageColumn.Width = new GridLength(200);
+                descriptionColumn.Width = new GridLength();
+
+                //image settings
+                Image agentLogo = new Image();
+                BitmapImage logo = new BitmapImage();
+
+                logo.BeginInit();
+                logo.UriSource = new Uri("/images/unknown.png", UriKind.Relative);
+                agentLogo.Stretch = Stretch.UniformToFill;
+                agentLogo.Source = logo;
+                logo.EndInit();
+                Grid.SetColumn(agentLogo, 0);
+
 
                 //border settings
-                agentDataBorder.Background = new SolidColorBrush(Color.FromArgb(0xFF, 0xC6, 0xD7, 0xFF));
-                agentDataBorder.Name = $"border{row}";
-                Grid.SetRow(agentDataBorder, row);
+                Border agentDataBorder = new Border();
+                Border agentLogoBorder = new Border();
+                agentDataBorder.Background = bgcolor;
+                agentLogoBorder.Background = bgcolor;
+                Grid.SetColumn(agentLogoBorder, 0);
+                Grid.SetColumn(agentDataBorder, 1);
+
 
                 //agentNameLabel settings
+                Label agentNameLabel = new Label();
+
                 agentNameLabel.Content = $"AgentName{row}";
                 agentNameLabel.HorizontalAlignment = HorizontalAlignment.Left;
                 agentNameLabel.VerticalAlignment = VerticalAlignment.Top;
                 agentNameLabel.Margin = new Thickness(10);
                 agentNameLabel.Width = Double.NaN;
-                Grid.SetRow(agentNameLabel, row);
+                Grid.SetColumn(agentNameLabel, 1);
+
 
                 //agentDiscountLabel settings
+                Label agentDiscountLabel = new Label();
+
                 int discount = row * 10;
                 agentDiscountLabel.Content = $"{discount}%";
                 agentDiscountLabel.HorizontalAlignment = HorizontalAlignment.Right;
                 agentDiscountLabel.VerticalAlignment = VerticalAlignment.Center;
                 agentDiscountLabel.Margin = new Thickness(20);
                 agentDiscountLabel.Width = Double.NaN;
-                Grid.SetRow(agentDiscountLabel, row);
+                Grid.SetColumn(agentDiscountLabel, 1);
+
 
                 //create instanses for grid
+                innerGrid.Children.Add(agentLogo);
+                innerGrid.Children.Add(agentLogoBorder);
+                innerGrid.Children.Add(agentDataBorder);
+                innerGrid.Children.Add(agentNameLabel);
+                innerGrid.Children.Add(agentDiscountLabel);
+
+
+                //inserting ready row in grid
+                RowDefinition rowDef = new RowDefinition();
+                rowDef.MinHeight = rowThikness;
+                rowDef.MaxHeight = rowThikness;
+                rowDef.Name = $"row{row}";
                 centerGrid.RowDefinitions.Add(rowDef);
-                centerGrid.Children.Add(agentDataBorder);
-                centerGrid.Children.Add(agentNameLabel);
-                centerGrid.Children.Add(agentDiscountLabel);
+                Grid.SetRow(innerGrid, row);
+                innerGrid.UpdateLayout();
+                centerGrid.Children.Add(innerGrid);
             }
             centerGrid.UpdateLayout();
         }
