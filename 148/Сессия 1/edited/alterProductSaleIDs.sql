f8244823b2ff64b13462eb7e04f9773f55d4febc
Derfuu <qwesterseven123456@gmail.com>
2022-10-18 14:45:30 +0300

    daleted useless files

--- 148/Сессия 1/edited/alterProductSaleIDs.sql
+++
@@ -1,7 +0,0 @@
-UPDATE salesTemp
-SET Продукция = (SELECT ID FROM Product WHERE Title = Продукция)
-GO
-
-UPDATE salesTemp
-SET [Наименование агента] = (SELECT ID FROM Agent WHERE Title = [Наименование агента])
-Go
--- 148/Сессия 1/edited/productSaleImport.sql
+++
@@ -1,3 +0,0 @@
-INSERT into ProductSale (ProductID, AgentID, SaleDate, ProductCount)
-SELECT * FROM salesTemp
-GO
